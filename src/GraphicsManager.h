e9f16f14b48da3db74dbd48a8689a4f9f4ca3476
andrziv <soveryhappyface@gmail.com>
2025-02-07 15:46:10 -0500

    Added more robust drawing system, allowing the enemies to be put ontop of the path instead of below it.

--- src/GraphicsManager.h
+++ src/GraphicsManager.h
@@ -2,44 +2,66 @@
 #define GRAPHICSMANAGER_H
 #include <SFML/Graphics.hpp>
 #include <list>
+#include <map>
 
 class GraphicsManager {
     std::unique_ptr<sf::RenderWindow> window;
-    std::list<std::shared_ptr<sf::Drawable>> drawables;
+    std::map<int, std::list<std::shared_ptr<sf::Drawable>>> drawables;
 
 public:
     GraphicsManager() {
         this->window = std::make_unique<sf::RenderWindow>(sf::RenderWindow(sf::VideoMode({1920, 1080}), "SFML works!"));
         this->window->setFramerateLimit(60);
+        drawables.insert(std::pair(0, std::list<std::shared_ptr<sf::Drawable>>()));
+        drawables.insert(std::pair(1, std::list<std::shared_ptr<sf::Drawable>>()));
+        drawables.insert(std::pair(2, std::list<std::shared_ptr<sf::Drawable>>()));
+        drawables.insert(std::pair(3, std::list<std::shared_ptr<sf::Drawable>>()));
+        drawables.insert(std::pair(4, std::list<std::shared_ptr<sf::Drawable>>()));
+    }
+
+    void addLowPriorityDrawable(const std::shared_ptr<sf::Drawable>& drawable) {
+        drawables.find(0)->second.push_back(drawable);
+    }
+
+    void addLowPriorityDrawables(const std::vector<std::shared_ptr<sf::Drawable>>& newDrawables) {
+        const auto drawableList = &drawables.find(0)->second;
+        for (const auto& drawable : newDrawables) {
+            drawableList->push_back(drawable);
+        }
     }
 
     void addDrawable(const std::shared_ptr<sf::Drawable>& drawable) {
-        drawables.push_front(drawable);
+        drawables.find(2)->second.push_back(drawable);
     }
 
     void addDrawables(const std::vector<std::shared_ptr<sf::Drawable>>& newDrawables) {
+        const auto drawableList = &drawables.find(2)->second;
         for (const auto& drawable : newDrawables) {
-            drawables.push_front(drawable);
+            drawableList->push_back(drawable);
         }
     }
 
     void addPriorityDrawable(const std::shared_ptr<sf::Drawable>& drawable) {
-        drawables.push_back(drawable);
+        drawables.find(4)->second.push_back(drawable);
     }
 
     void addPriorityDrawables(const std::vector<std::shared_ptr<sf::Drawable>>& newDrawables) {
+        const auto drawableList = &drawables.find(4)->second;
         for (const auto& drawable : newDrawables) {
-            drawables.push_back(drawable);
+            drawableList->push_back(drawable);
         }
     }
 
     void removeDrawable(const std::shared_ptr<sf::Drawable>& toRemove) {
-        drawables.remove(toRemove);
+        for (auto &[priority, listOfDrawables] : drawables) {
+            listOfDrawables.remove(toRemove);
+            return;
+        }
     }
 
     void removeDrawables(const std::vector<std::shared_ptr<sf::Drawable>>& toRemove) {
         for (const std::shared_ptr<sf::Drawable>& drawable : toRemove) {
-            drawables.remove(drawable);
+            removeDrawable(drawable);
         }
     }
 
@@ -57,8 +79,10 @@ public:
 
     void draw() const {
         window->clear();
-        for (const auto& drawable : drawables) {
-            window->draw(*drawable);
+        for (auto &[priority, listOfDrawables] : drawables) {
+            for (const auto& drawable : listOfDrawables) {
+                window->draw(*drawable);
+            }
         }
         window->display();
     }
